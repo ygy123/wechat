6e3163ae12115e7e855baf8b149ecd5c26a4bf36
ygy <ygy@qq.com>
2016-10-28 09:21:38 +0800

    1

---
+++ src/Wechat/WechatServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+namespace Wechat;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
+
+/**
+ *
+ * LaravelExcel Excel ServiceProvider
+ *
+ * @category   Laravel Excel
+ * @package    maatwebsite/excel
+ * @copyright  Copyright (c) 2013 - 2014 Maatwebsite (http://www.maatwebsite.nl)
+ * @author     Maatwebsite <info@maatwebsite.nl>
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
+ */
+class WechatServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+
+}
